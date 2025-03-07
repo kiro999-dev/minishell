d08961a466c6402681d93f6da17470bf998c112d
Oussama Najem <onajem@e2r11p2.1337.ma>
2025-03-07 17:19:02 +0000

    cd init

---
+++ execution/cd.c
@@ -0,0 +1,12 @@
+#include "../minishell.h"
+#include "heper.h"
+
+
+
+int main(int ac, char *av[], char *env[])
+{
+    t_env vars;
+
+    vars.env = env;
+    
+}
---
+++ execution/heper.h
@@ -0,0 +1,13 @@
+
+
+
+#ifndef HELPER_H
+#define HELPER_H
+
+typedef struct s_env
+{
+    char **env;
+}t_env;
+
+
+#endif
