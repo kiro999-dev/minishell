0109a469e562b01cd6ac3aada3d6819a7254e433
HEIZENBERG <65958229+HEIZENNBERG@users.noreply.github.com>
2025-05-07 16:20:18 +0100

    fix ambigous

--- execution/files_utils.c
+++ execution/files_utils.c
@@ -28,13 +28,10 @@ int	check_in_out(t_file *file, int red)
 int	apply_input_redirection(int *last_input_fd, t_file *file)
 {
 	if (*last_input_fd != -1)
-	{
-		*last_input_fd = -1;
 		close(*last_input_fd);
-	}
 	if (file->file && file->ambigous != 1)
 		*last_input_fd = open(file->file, O_RDONLY);
-	if (*last_input_fd == -1)
+	if (*last_input_fd == -1 || file->ambigous == 1)
 	{
 		if (file->ambigous)
 			print_error(file->file, "ambiguous redirect\n", NULL);
@@ -48,15 +45,12 @@ int	apply_input_redirection(int *last_input_fd, t_file *file)
 int	apply_output_redirection(int *last_out, t_file *file, int single)
 {
 	if (*last_out != -1 && single == 1)
-	{
-		*last_out = -1;
 		close(*last_out);
-	}
 	if (file->type == IS_FILE_OUT && file->ambigous != 1)
 		*last_out = open(file->file, O_RDWR | O_CREAT | O_TRUNC, 0644);
 	else if (file->type == IS_FILE_APPEND && file->ambigous != 1)
 		*last_out = open(file->file, O_RDWR | O_CREAT | O_APPEND, 0644);
-	if (*last_out == -1)
+	if (*last_out == -1 || file->ambigous == 1)
 	{
 		if (file->ambigous)
 			print_error(file->file, " ambiguous redirect\n", NULL);
